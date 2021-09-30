809e66a7ee7c1091b35e1497c6db46609e72c56f
Tosin Akinosho <takinosh@redhat.com>
2021-09-30 18:57:27 -0400

    updating setup script

--- configure-acm-deployment-local-cluster.sh
+++ configure-acm-deployment-local-cluster.sh
@@ -9,5 +9,7 @@ oc create -f acm-configs/amq_placement_local_cluster.yaml
 oc create -f acm-configs/rhpam_placement_local_cluster.yaml 
 oc create -f acm-configs/amq_subscription_local_cluster.yaml
 oc create -f acm-configs/pam_subscription_local_cluster.yaml
+echo "****************************************************************"
 echo "WAIT for the operators to deploy before running the command below"
+echo "****************************************************************"
 echo "oc create -f acm-configs/workload_subscription_local_cluster.yaml"
